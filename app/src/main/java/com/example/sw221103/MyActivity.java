ffb516c693ff2ea63a3fbc3b757efc698bb0c42e
seo-jy <seoji019@gmail.com>
2022-11-28 21:52:28 +0900

    .

---
+++ app/src/main/java/com/example/sw221103/MyActivity.java
@@ -0,0 +1,49 @@
+package com.example.sw221103;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MyActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView textViewUserEmail;
+    private FirebaseAuth firebaseAuth;
+    private Button buttonLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_activity);
+
+
+        textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //textViewUserEmail의 내용을 변경해 준다.
+        textViewUserEmail.setText("반갑습니다.\n"+ user.getEmail()+"님!");
+
+        buttonLogout.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == buttonLogout) {
+            firebaseAuth.signOut();
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+}
+
