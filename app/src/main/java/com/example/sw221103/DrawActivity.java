d0bf18a4b67683b09fb2d27aa6ebf1a093b60197
seo-jy <seoji019@gmail.com>
2023-03-01 22:53:24 +0900

    .

---
+++ app/src/main/java/com/example/sw221103/DrawActivity.java
@@ -0,0 +1,101 @@
+package com.example.sw221103;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+public class DrawActivity extends AppCompatActivity {
+
+    MyPaintView view;
+    int tColor,n=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.draw_main);
+        view = new MyPaintView(this);
+
+        LinearLayout container = findViewById(R.id.container);
+        Resources res = getResources();
+
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        container.addView(view, params);
+
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+
+
+        Button btn=findViewById(R.id.colorPickerButton);
+        Button btn2=findViewById(R.id.thickPickerButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+            }
+        });
+    }
+
+    private void show() {
+        final EditText editText=new EditText(this);
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("AlertDialog Title");
+        builder.setMessage("굵기 입력");
+        builder.setView(editText);
+        builder.setPositiveButton("입력",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        view.setStrokeWidth(Integer.parseInt(editText.getText().toString()));
+
+                    }
+                });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.show();
+
+    }
+
+    private void openColorPicker() {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, tColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                Toast.makeText(getApplicationContext(),""+tColor,Toast.LENGTH_LONG).show();
+                view.setColor(color);
+            }
+        });
+        colorPicker.show();
+    }
+
+}
--- app/src/main/java/com/example/sw221103/HomeActivity.java
+++ app/src/main/java/com/example/sw221103/HomeActivity.java
@@ -22,77 +22,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-/*
-        Button study_button = (Button) findViewById(R.id.Pen_button);
-        study_button.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), study_activity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button delivery_button = (Button) findViewById(R.id.Deli_button);
-        delivery_button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), delivery_activity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button exercise_button = (Button) findViewById(R.id.Exer_button);
-        exercise_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), exercise_activity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button together_button = (Button) findViewById(R.id.Tog_button);
-        together_button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), together_activity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button shopping_button = (Button) findViewById(R.id.Shop_button);
-        shopping_button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), shopping_activity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button eat_button = (Button) findViewById(R.id.eat_button);
-        eat_button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), eat_activity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button next_button = (Button) findViewById(R.id.next_button);
-        next_button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TodoActivity.class);
-                startActivity(intent);
-            }
-        });
-
- */
         Button TODO = (Button) findViewById(R.id.todo);
         TODO.setOnClickListener(new View.OnClickListener() {
 
@@ -103,16 +33,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        ImageView main = (ImageView) findViewById(R.id.imageView3);
-        main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
+/*
         Button butstudy = (Button) findViewById(R.id.butstudy);
         butstudy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -121,15 +42,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
             }
         });
-
-        Button buttonTest = (Button) findViewById(R.id.test);
-        buttonTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, Test.class);
-                startActivity(intent);
-            }
-        });
+ */
 
         Button board = (Button) findViewById(R.id.board);
         board.setOnClickListener(new View.OnClickListener() {
@@ -140,12 +53,30 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        Button draw = (Button) findViewById(R.id.draw);
+        draw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, DrawActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         Button buttonMy = (Button) findViewById(R.id.buttonMy);
         buttonMy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(HomeActivity.this, MyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button home = (Button) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
